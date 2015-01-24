0086f17aecc58265b896dac6a4b2976b4869f68e
Marcel Hollerbach <marcel-hollerbach@t-online.de>
2015-01-24 12:58:48 +0100

    settingswidget test: Added a buttom to test delete items.

--- src/bin/test_settingspane.c
+++ src/bin/test_settingspane.c
@@ -192,8 +192,18 @@ save_q_cb(void *data, Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNUSED
      printf("No.\n");
    else
      printf("Yes :)\n");
-
 }
+
+static void
+del_focused_cb(void *data, Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNUSED)
+{
+  Elm_Settingspane_Item *item;
+
+  item = elm_settingspane_focused_get(data);
+
+  elm_settingspane_item_delete(item);
+}
+
 
 void
 test_settingspane(void *data EINA_UNUSED, Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNUSED)
@@ -279,6 +289,14 @@ test_settingspane(void *data EINA_UNUSED, Evas_Object *obj EINA_UNUSED, void *ev
    evas_object_size_hint_weight_set(o, EVAS_HINT_EXPAND, 0.0);
    evas_object_size_hint_align_set(o, EVAS_HINT_FILL, EVAS_HINT_FILL);
    evas_object_smart_callback_add(o, "clicked", save_q_cb, sw);
+   elm_box_pack_end(btnbox, o);
+   evas_object_show(o);
+
+   o = elm_button_add(win);
+   elm_object_text_set(o, "Delete focused item");
+   evas_object_size_hint_weight_set(o, EVAS_HINT_EXPAND, 0.0);
+   evas_object_size_hint_align_set(o, EVAS_HINT_FILL, EVAS_HINT_FILL);
+   evas_object_smart_callback_add(o, "clicked", del_focused_cb, sw);
    elm_box_pack_end(btnbox, o);
    evas_object_show(o);
 
