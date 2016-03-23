e87858cc31e4e03cdb0cac08d0e15152efdd2be6
Amitesh Singh <amitesh.sh@samsung.com>
2016-03-23 12:27:04 +0530

    slider: add test for range

--- src/bin/test_slider.c
+++ src/bin/test_slider.c
@@ -61,7 +61,7 @@ _step_size_calculate(double min, double max)
 void
 test_slider(void *data EINA_UNUSED, Evas_Object *obj EINA_UNUSED, void *event_info EINA_UNUSED)
 {
-   Evas_Object *win, *fr, *bx, *sl, *ic, *sl1, *bx2, *bt;
+   Evas_Object *win, *fr, *bx, *sl, *ic, *sl1, *bx2, *bx3, *bt;
    double step;
    char buf[PATH_MAX];
 
@@ -164,22 +164,6 @@ test_slider(void *data EINA_UNUSED, Evas_Object *obj EINA_UNUSED, void *event_in
    evas_object_smart_callback_add(sl, "delay,changed", _delay_change_cb, NULL);
    elm_box_pack_end(bx, sl);
    evas_object_show(sl);
-
-   // normal range slider
-   sl = elm_slider_add(bx);
-   elm_object_text_set(sl, "Range");
-   elm_object_part_content_set(sl, "icon", ic);
-   elm_slider_unit_format_set(sl, "%1.1f units");
-   elm_slider_indicator_format_set(sl, "%1.1f");
-   elm_slider_span_size_set(sl, 120);
-   evas_object_size_hint_align_set(sl, EVAS_HINT_FILL, 0.5);
-   evas_object_size_hint_weight_set(sl, EVAS_HINT_EXPAND, 0.0);
-   elm_slider_range_enable_set(sl, EINA_TRUE);
-   elm_slider_range_value_set(sl, 0.3, 0.8);
-   elm_box_pack_end(bx, sl);
-   evas_object_show(ic);
-   evas_object_show(sl);
-
 
    // disabled horizontally inverted slider
    ic = elm_icon_add(bx);
@@ -288,24 +272,6 @@ test_slider(void *data EINA_UNUSED, Evas_Object *obj EINA_UNUSED, void *event_in
    elm_box_pack_end(bx2, sl);
    evas_object_show(sl);
 
-   // range vertical slider
-   sl = elm_slider_add(bx2);
-   elm_object_text_set(sl, "Vertical Range");
-   elm_slider_unit_format_set(sl, "%1.1f units");
-   elm_slider_indicator_format_set(sl, "%1.1f");
-   elm_slider_span_size_set(sl, 60);
-   evas_object_size_hint_align_set(sl, 0.5, EVAS_HINT_FILL);
-   evas_object_size_hint_weight_set(sl, 0.0, EVAS_HINT_EXPAND);
-   elm_slider_indicator_show_set(sl, EINA_TRUE);
-   elm_slider_value_set(sl, 0.7);
-   elm_slider_horizontal_set(sl, EINA_FALSE);
-   evas_object_smart_callback_add(sl, "changed", _change_print_cb, sl);
-   elm_slider_range_enable_set(sl, EINA_TRUE);
-   elm_slider_range_value_set(sl, 0.4, 0.7);
-   elm_box_pack_end(bx2, sl);
-   evas_object_show(sl);
-
-
    // box for bottom buttons
    bx2 = elm_box_add(win);
    evas_object_size_hint_weight_set(bx2, EVAS_HINT_EXPAND, 0.0);
@@ -336,6 +302,152 @@ test_slider(void *data EINA_UNUSED, Evas_Object *obj EINA_UNUSED, void *event_in
    evas_object_smart_callback_add(bt, "clicked", _bt_m1, sl);
    evas_object_show(bt);
    elm_box_pack_end(bx2, bt);
+
+   fr = elm_frame_add(bx);
+   elm_object_text_set(fr, "Range");
+   evas_object_size_hint_weight_set(fr, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   elm_box_pack_end(bx, fr);
+   evas_object_show(fr);
+
+   bx3 = elm_box_add(fr);
+   evas_object_size_hint_weight_set(bx3, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   evas_object_show(bx3);
+   elm_object_content_set(fr, bx3);
+
+   //Disabled
+   sl = elm_slider_add(bx3);
+   elm_object_text_set(sl, "Disabled");
+   elm_slider_unit_format_set(sl, "%1.1f units");
+   elm_slider_span_size_set(sl, 120);
+   elm_slider_min_max_set(sl, 50, 150);
+   evas_object_size_hint_align_set(sl, EVAS_HINT_FILL, 0.5);
+   evas_object_size_hint_weight_set(sl, EVAS_HINT_EXPAND, 0.0);
+   elm_object_disabled_set(sl, EINA_TRUE);
+   elm_slider_range_enable_set(sl, EINA_TRUE);
+   elm_slider_range_value_set(sl, 20, 100);
+   elm_box_pack_end(bx3, sl);
+   evas_object_show(sl);
+
+   // horizontal slider with range
+   sl = elm_slider_add(bx3);
+   evas_object_size_hint_weight_set(sl, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+   elm_slider_indicator_show_set(sl, EINA_TRUE);
+   elm_object_text_set(sl, "Horizontal");
+   elm_slider_unit_format_set(sl, "%1.5f units");
+   elm_slider_indicator_format_set(sl, "%1.5f");
+   elm_slider_span_size_set(sl, 160);
+   elm_slider_value_set(sl, 0.7);
+   elm_slider_range_enable_set(sl, EINA_TRUE);
+   elm_slider_range_value_set(sl, 0.4, 0.9);
+   elm_box_pack_end(bx3, sl);
+   evas_object_show(sl);
+
+   sl = elm_slider_add(bx3);
+   elm_slider_unit_format_set(sl, "%1.0f units");
+   elm_slider_indicator_format_set(sl, "%1.0f");
+   elm_slider_span_size_set(sl, 120);
+   elm_slider_min_max_set(sl, 0, 9);
+   elm_object_text_set(sl, "Manual step");
+   step = _step_size_calculate(0, 9);
+   elm_slider_step_set(sl, step);
+   evas_object_size_hint_align_set(sl, EVAS_HINT_FILL, 0.5);
+   evas_object_size_hint_weight_set(sl, EVAS_HINT_EXPAND, 0.0);
+   elm_slider_range_enable_set(sl, EINA_TRUE);
+   elm_box_pack_end(bx3, sl);
+   evas_object_show(sl);
+
+   sl = elm_slider_add(bx3);
+   elm_slider_unit_format_set(sl, "%1.0f units");
+   elm_slider_indicator_format_set(sl, "%1.0f");
+   elm_slider_span_size_set(sl, 120);
+   elm_slider_min_max_set(sl, 0, 100);
+   elm_object_text_set(sl, "Show Indicator on Focus: ");
+   step = _step_size_calculate(0, 9);
+   elm_slider_step_set(sl, step);
+   elm_slider_indicator_show_on_focus_set(sl, EINA_TRUE);
+   evas_object_size_hint_align_set(sl, EVAS_HINT_FILL, 0.5);
+   evas_object_size_hint_weight_set(sl, EVAS_HINT_EXPAND, 0.0);
+   elm_slider_range_enable_set(sl, EINA_TRUE);
+   elm_box_pack_end(bx3, sl);
+   evas_object_show(sl);
+
+   // normal horizontal slider
+   ic = elm_icon_add(bx3);
+   snprintf(buf, sizeof(buf), "%s/images/logo_small.png", elm_app_data_dir_get());
+   elm_image_file_set(ic, buf, NULL);
+   evas_object_size_hint_aspect_set(ic, EVAS_ASPECT_CONTROL_VERTICAL, 1, 1);
+
+   sl = elm_slider_add(bx3);
+   elm_object_text_set(sl, "Horizontal");
+   elm_object_part_content_set(sl, "icon", ic);
+   elm_slider_unit_format_set(sl, "%1.1f units");
+   elm_slider_indicator_format_set(sl, "%1.1f");
+   elm_slider_span_size_set(sl, 120);
+   evas_object_size_hint_align_set(sl, EVAS_HINT_FILL, 0.5);
+   evas_object_size_hint_weight_set(sl, EVAS_HINT_EXPAND, 0.0);
+   elm_slider_range_enable_set(sl, EINA_TRUE);
+   elm_box_pack_end(bx3, sl);
+   evas_object_show(sl);
+
+
+   ic = elm_icon_add(bx3);
+   snprintf(buf, sizeof(buf), "%s/images/logo_small.png", elm_app_data_dir_get());
+   elm_image_file_set(ic, buf, NULL);
+   evas_object_size_hint_aspect_set(ic, EVAS_ASPECT_CONTROL_VERTICAL, 1, 1);
+
+   sl = elm_slider_add(bx3);
+   elm_object_text_set(sl, "Horizontal inverted");
+   elm_object_part_content_set(sl, "end", ic);
+   elm_slider_unit_format_set(sl, "%3.0f units");
+   elm_slider_span_size_set(sl, 80);
+   elm_slider_indicator_format_set(sl, "%3.0f");
+   elm_slider_min_max_set(sl, 50, 150);
+   elm_slider_value_set(sl, 80);
+   elm_slider_inverted_set(sl, EINA_TRUE);
+   evas_object_size_hint_align_set(sl, 0.5, 0.5);
+   evas_object_size_hint_weight_set(sl, 0.0, 0.0);
+   //TODO:
+   //evas_object_smart_callback_add(sl, "delay,changed", _delay_change_cb, NULL);
+   elm_slider_range_enable_set(sl, EINA_TRUE);
+   elm_box_pack_end(bx3, sl);
+   evas_object_show(sl);
+
+   ic = elm_icon_add(bx3);
+   snprintf(buf, sizeof(buf), "%s/images/logo_small.png", elm_app_data_dir_get());
+   elm_image_file_set(ic, buf, NULL);
+   evas_object_size_hint_aspect_set(ic, EVAS_ASPECT_CONTROL_VERTICAL, 1, 1);
+
+   sl = elm_slider_add(bx3);
+   elm_object_text_set(sl, "Disabled inverted");
+   elm_object_part_content_set(sl, "end", ic);
+   elm_slider_span_size_set(sl, 80);
+   elm_slider_indicator_format_set(sl, "%3.0f");
+   elm_slider_min_max_set(sl, 50, 150);
+   elm_slider_value_set(sl, 80);
+   elm_slider_inverted_set(sl, EINA_TRUE);
+   elm_object_disabled_set(sl, EINA_TRUE);
+   evas_object_size_hint_align_set(sl, 0.5, 0.5);
+   evas_object_size_hint_weight_set(sl, 0.0, 0.0);
+   elm_slider_range_enable_set(sl, EINA_TRUE);
+   elm_slider_inverted_set(sl, EINA_TRUE);
+   elm_box_pack_end(bx3, sl);
+   evas_object_show(sl);
+
+   sl = elm_slider_add(bx3);
+   elm_slider_indicator_show_set(sl, EINA_FALSE);
+   elm_object_text_set(sl, "Scale doubled");
+   elm_slider_unit_format_set(sl, "%3.0f units");
+   elm_slider_span_size_set(sl, 40);
+   evas_object_size_hint_align_set(sl, EVAS_HINT_FILL, 0.5);
+   evas_object_size_hint_weight_set(sl, EVAS_HINT_EXPAND, 0.0);
+   elm_slider_indicator_format_set(sl, "%3.0f");
+   elm_slider_min_max_set(sl, 50, 150);
+   elm_slider_value_set(sl, 80);
+   elm_object_scale_set(sl, 2.0);
+   elm_slider_range_enable_set(sl, EINA_TRUE);
+   elm_slider_range_value_set(sl, 100, 145);
+   elm_box_pack_end(bx3, sl);
+   evas_object_show(sl);
 
    evas_object_show(win);
 }
